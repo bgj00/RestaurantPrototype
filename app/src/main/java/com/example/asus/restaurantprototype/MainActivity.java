2ac186e922c3128e681f139fb9a75d4a0fdc0c07
Bagja <segiadis@gmail.com>
2017-07-04 09:27:39 +0700

    add new layout main menu

--- app/src/main/java/com/example/asus/restaurantprototype/MainActivity.java
+++ app/src/main/java/com/example/asus/restaurantprototype/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.asus.restaurantprototype;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void login(View view){
+        Intent intent = new Intent(MainActivity.this, menuUtama.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/asus/restaurantprototype/menuUtama.java
@@ -0,0 +1,13 @@
+package com.example.asus.restaurantprototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class menuUtama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_utama);
+    }
+}
